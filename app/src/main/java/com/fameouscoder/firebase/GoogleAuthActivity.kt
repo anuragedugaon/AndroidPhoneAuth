f64b3a6bfc933541ff2f8d0a016cb5f6097788c0
anuragkumaredugaon <anuragkumar.edugaon@gmail.com>
2023-06-15 13:16:54 +0530

    hjjsj

---
+++ app/src/main/java/com/fameouscoder/firebase/GoogleAuthActivity.kt
@@ -0,0 +1,11 @@
+package com.fameouscoder.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_auth)
+    }
+}
